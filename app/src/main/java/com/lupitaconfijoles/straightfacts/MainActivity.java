fc440b78365628763456735c7bde1c82762ee2fc
LupitaConFrijoles <7450639+Lady-Esoteric@users.noreply.github.com>
2018-04-23 16:11:50 -0400

    stage 3
    I have added questions and will work on the layout of the screen along with the scroing methods so that it can be fully functional

--- app/src/main/java/com/lupitaconfijoles/straightfacts/MainActivity.java
+++ app/src/main/java/com/lupitaconfijoles/straightfacts/MainActivity.java
@@ -2,6 +2,8 @@ package com.lupitaconfijoles.straightfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//make sure to add a grading logic for the questions and the right answers
+
+
+public void onRadioButtonClicked(View view) {
+    boolean checked = ((RadioButton)view).isChecked();
+
+    switch(view.getId()){
+        case R.id.answr6True:
+            if (checked)
+                break;
+    }
 }
+//make a toast message for the submit button
+//  Toast.makeText(getApplicationContext(), "Your Score is " + Score, Toast.LENGTH_LONG).show();
+
+
+}
+
+
+
