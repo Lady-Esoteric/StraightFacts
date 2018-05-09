fdd98eb4fcc73658e4426092facc4ecf46eb9368
LupitaConFrijoles <7450639+Lady-Esoteric@users.noreply.github.com>
2018-05-09 07:58:50 -0400

    Clean up time
    This is to make sure that the display is more clean looking and can be looking a bit nicer when the screen rotates.

--- app/src/main/java/com/lupitaconfijoles/straightfacts/MainActivity.java
+++ app/src/main/java/com/lupitaconfijoles/straightfacts/MainActivity.java
@@ -2,33 +2,37 @@ package com.lupitaconfijoles.straightfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.EditText;
 import android.view.View;
+import android.widget.TextView;
+import android.text.Editable;
+import java.text.NumberFormat;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-//user starting sccore
+/*user starting score will be listed
+so that it will have a minimum
+ */
     int userScore = 0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+      protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 //make sure to add a grading logic for the questions and the right answers
-public void updateUserScore(View v){
-        userScore = userScore + 2;
+//public void updateUserScore(View v){
+       // userScore = userScore + 2
 
-public void onRadioButtonClicked(View v) {
-    return userScore;
-    }
-public void
 }
 //make a toast message for the submit button
 //public void
 //  Toast.makeText(getApplicationContext(), "Your Score is " + Score, Toast.LENGTH_LONG).show();
 
 
-}
+
 
 
 
